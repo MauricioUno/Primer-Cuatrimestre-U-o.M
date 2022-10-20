795292c616e1e0bb1b08954ef50c2484018616b1
MauricioUno <mauriciouno37@gmail.com>
2022-10-20 18:25:55 -0300

    Actualizacion Pygame

---
+++ Programas Pygame/Test_Pygame/test_pygame.py
@@ -0,0 +1,31 @@
+import pygame.locals
+import pygame
+
+pygame.init()
+
+running = True
+
+window = pygame.display.set_mode((500, 500), 0, 32)
+pygame.display.set_caption("COCO!")
+
+imagen_espacio = pygame.image.load("Programas Pygame\Espacio.jpg")
+window.blit(imagen_espacio, (0,0))
+
+i = 0
+while(running):
+    window.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    teclas_presionadas = pygame.key.get_pressed()
+    if True in teclas_presionadas:
+        if teclas_presionadas[pygame.K_DOWN]:
+            pygame.draw.rect(window,(0,100,0),(i,0,50,100))
+            i += 1
+
+    
+    pygame.display.flip()
+
+
+pygame.quit()
